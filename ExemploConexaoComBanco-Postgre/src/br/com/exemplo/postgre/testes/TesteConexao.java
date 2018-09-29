7889d6ddf575513e04f71fb295425e817c3ce02e
Gustavo Ferreira <gustavofvieira@live.com>
2018-09-28 21:54:56 -0300

    conexão com postgre

---
+++ ExemploConexaoComBanco-Postgre/src/br/com/exemplo/postgre/testes/TesteConexao.java
@@ -0,0 +1,12 @@
+package br.com.exemplo.postgre.testes;
+import br.com.exemplo.postgre.Conexao; 
+
+public class TesteConexao {
+
+	public static void main(String[] args) {
+		Conexao.getConnection();
+		// TODO Auto-generated method stub
+
+	}
+
+}
